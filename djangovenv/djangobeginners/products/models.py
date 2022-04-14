342fb623e447fce10c235b31f1c23c8e944920dc
Jacob Graham <jakegraham54@gmail.com>
2022-04-14 13:23:13 -0400

    Changed several database fields and their parameters in products/models.py folder

--- djangovenv/djangobeginners/products/models.py
+++ djangovenv/djangobeginners/products/models.py
@@ -6,7 +6,8 @@ from django.db import models
 # py manage.py is the start of every command that will be used with the django project.
 # py manage.py makemigrations and py manage.py migrate must be run every time models.py is changed in a component folder to sync the application database with the model class
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='This is cool')
+    title = models.CharField(max_length=120) # max_length is required when using a CharField
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=1000)
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField(null=True, default=True)
